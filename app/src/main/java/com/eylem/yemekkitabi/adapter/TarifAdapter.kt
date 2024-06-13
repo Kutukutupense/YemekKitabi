1fc0145bc06f356599c84694a092a06c8c068a4f
Eylem <161935446+Kutukutupense@users.noreply.github.com>
2024-06-13 16:18:47 +0300

    kotlin yemek tarifleri kitabi

---
+++ app/src/main/java/com/eylem/yemekkitabi/adapter/TarifAdapter.kt
@@ -0,0 +1,35 @@
+package com.eylem.yemekkitabi.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.eylem.yemekkitabi.databinding.RecyclerRowBinding
+import com.eylem.yemekkitabi.model.Tarif
+import com.eylem.yemekkitabi.view.ListeFragmentDirections
+
+class TarifAdapter( val tarifListesi : List<Tarif>) : RecyclerView.Adapter<TarifAdapter.TarifHolder>() {
+
+    class TarifHolder (val binding: RecyclerRowBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TarifHolder {
+       val recyclerRowBinding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return TarifHolder(recyclerRowBinding)
+    }
+
+    override fun getItemCount(): Int {
+        return tarifListesi.size
+    }
+
+    override fun onBindViewHolder(holder: TarifHolder, position: Int) {
+        holder.binding.recyclerViewTextView.text = tarifListesi[position].isim
+        holder.itemView.setOnClickListener {
+            val action = ListeFragmentDirections.actionListeFragmentToTarifFragment(bilgi = "eski",
+                id = tarifListesi[position].id)
+            Navigation.findNavController(it).navigate(action)
+
+        }
+    }
+}
